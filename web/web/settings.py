5b893db43cec5fc8a2a3dd342b8ea8840febf274
zhanglun <zhanglun@guoshujinfu.com>
2019-10-12 17:43:46 +0800

    :bug: fix sqlite not working in django 2.2

--- web/web/settings.py
+++ web/web/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': ENV('DB_NAME'),
+        'USER': ENV('DB_USER'),
+        'PASSWORD': ENV('DB_PASSWORD'),
+        'HOST': ENV('DB_HOST'),
+        'PORT': ENV('DB_PORT'),
     }
 }
 
