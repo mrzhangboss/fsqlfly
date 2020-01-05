2d5071e5bfd722f98f692391c92923daa8e3d3cb
mrzhangboss <2529450174@qq.com>
2020-01-05 12:15:03 +0800

    :star: support localhost not need login

--- web/api/middleware.py
+++ web/api/middleware.py
@@ -1,6 +1,16 @@
 # -*- coding:utf-8 -*-
+from typing import Optional
 from django.contrib.auth.views import redirect_to_login
 from django.http import HttpRequest, HttpResponse, JsonResponse
+
+
+def get_client_ip(request: HttpRequest) -> Optional[str]:
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 
 class LoginCheckMiddleware(object):
@@ -8,7 +18,7 @@ class LoginCheckMiddleware(object):
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
-        if not request.user.id:
+        if (not request.user.id) and (get_client_ip(request) not in ('127.0.0.1', 'localhost')):
             if (not request.path.startswith('/admin')) and (not request.path == '/'):
                 return JsonResponse(data=dict(code=500, msg='need login', data=None))
         # Continue processing the request as usual:
