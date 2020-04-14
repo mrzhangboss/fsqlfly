eda8e66a980ea8d046b3e7f80c1d2e2af4730071
mrzhangboss <2529450174@qq.com>
2020-04-14 15:06:26 +0800

    :bug: fix api response code error

--- fsqlfly/utils/response.py
+++ fsqlfly/utils/response.py
@@ -4,5 +4,5 @@ from typing import Optional, Union
 from fsqlfly.utils.strings import dict2camel
 
 
-def create_response(data: Union[Optional[dict], list] = None, code: int = 0, msg: str = '') -> dict:
+def create_response(data: Union[Optional[dict], list] = None, code: int = 200, msg: str = '') -> dict:
     return dict2camel({'data': data, 'code': code, 'success': code == 200, 'msg': msg})
