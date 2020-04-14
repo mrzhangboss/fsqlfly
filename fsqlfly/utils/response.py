878b99d61cb8636104623e477db9a69544ad0f77
mrzhangboss <2529450174@qq.com>
2020-04-14 14:51:29 +0800

    :bug: fix api success error

--- fsqlfly/utils/response.py
+++ fsqlfly/utils/response.py
@@ -5,4 +5,4 @@ from fsqlfly.utils.strings import dict2camel
 
 
 def create_response(data: Union[Optional[dict], list] = None, code: int = 0, msg: str = '') -> dict:
-    return dict2camel({'data': data, 'code': code, 'success': code == 0, 'msg': msg})
+    return dict2camel({'data': data, 'code': code, 'success': code == 200, 'msg': msg})
