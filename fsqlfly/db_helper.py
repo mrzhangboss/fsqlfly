13d333f6fcf57a40948bee9e8eb49f2a968d5d0a
mrzhangboss <2529450174@qq.com>
2020-04-23 23:59:29 +0800

    :star: support filter decorator

--- fsqlfly/db_helper.py
+++ fsqlfly/db_helper.py
@@ -1,20 +1,53 @@
 from fsqlfly.db_models import *
-from fsqlfly.common import DBRes
+from functools import wraps
+from typing import Callable, Type
+from fsqlfly.common import DBRes, RespCode
+
+ALL_SUPPORT_MODELS = {
+    'connection': Connection
+}
+
+
+class SupportModel:
+    def __contains__(self, item):
+        return item not in ALL_SUPPORT_MODELS
+
+    def __call__(self, item) -> Type[Base]:
+        return ALL_SUPPORT_MODELS[item]
+
+
+SUPPORT_MODELS = SupportModel()
+
+
+def filter_not_support(func: Callable) -> Callable:
+    @wraps(func)
+    def _call_(*args, **kwargs):
+        model = kwargs['model'] if 'model' in kwargs else args[1]
+        if model not in SUPPORT_MODELS:
+            return DBRes(code=RespCode.APIFail.code, msg=f'{model} not support')
+
+        return func(*args, **kwargs)
+
+    return _call_
 
 
 class DBDao(object):
     @classmethod
-    def update(cls, pk: int, model: str, obj: dict) -> DBRes:
-        pass
+    @filter_not_support
+    def update(cls, model: str, pk: int, obj: dict) -> DBRes:
+        print(obj)
 
     @classmethod
+    @filter_not_support
     def create(cls, model: str, obj: dict) -> DBRes:
         pass
 
     @classmethod
+    @filter_not_support
     def get(cls, model: str) -> DBRes:
         pass
 
     @classmethod
-    def delete(cls, pk: int, model: str) -> DBRes:
+    @filter_not_support
+    def delete(cls, model: str, pk: int) -> DBRes:
         pass
--- fsqlfly/tests/test_db_helper.py
+++ fsqlfly/tests/test_db_helper.py
@@ -1,13 +1,26 @@
 # -*- coding:utf-8 -*-
 from __future__ import absolute_import, unicode_literals, print_function
 import unittest
-
 from unittest.mock import patch, Mock
+from fsqlfly.db_helper import DBDao
+from fsqlfly.common import RespCode
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_not_support(self):
+        s_m = 'connection'
+        with self.assertRaises(TypeError):
+            DBDao.update(1, model=s_m, obj=dict())
+
+        self.assertEqual(DBDao.update('ttttasdf', 1, dict()).success, False)
+        self.assertEqual(DBDao.update('ttttasdf', pk=1, obj=dict()).success, False)
+        self.assertEqual(DBDao.update(model='ttttasdf', pk=1, obj=dict()).success, False)
+        self.assertEqual(DBDao.update(model='ttttasdf', pk=1, obj=dict()).success, False)
+
+        self.assertEqual(DBDao.update(model=s_m, pk=1, obj=dict()).success, False)
+
+        self.assertEqual(DBDao.get('ttttasdf').code, RespCode.APIFail.code)
+        self.assertEqual(DBDao.get('ttttasdf').success, False)
 
 
 if __name__ == '__main__':
